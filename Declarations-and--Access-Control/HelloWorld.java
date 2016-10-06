81d1d5aa79d4cd7371d3a6103316cb1fbc58e33b
zazzel <zazzel.cvs@gmail.com>
2016-10-06 16:55:13 +0530

    Create HelloWorld.java

---
+++ Declarations-and--Access-Control/HelloWorld.java
@@ -0,0 +1,16 @@
+package basic.java;
+
+//simplest program to start off
+
+public class HelloWorld {
+		
+	public static void main(String[] args) {
+		/*There will be only one main() which will be call by JVM and 
+		 * if we overrride main method then we have to call it ourself*/
+		/*main() access modifier is public so it can be access from anywhere, 
+		 * static will load this method into memory at first and 
+		 * void will be responsible for returning nothing.*/
+		System.out.println("hello world");
+	}
+
+}
